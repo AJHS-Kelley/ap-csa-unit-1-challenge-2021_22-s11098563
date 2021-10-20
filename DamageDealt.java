08d7a30ccf989e68117c3acf5f57346fb1d7586e
Cook <s11098563@students.duvalschools.org>
2021-10-20 08:35:58 -0400

    [5B] v0.4
    Created a new scanner called myScanner.

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,9 +1,12 @@
-// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-20-2021, 8:32am, v0.3
+// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-20-2021, 8:35am, v0.4
 
 import java.util.Scanner;
 
 public class DamageDealt {
     public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+
+
 
     }
 }
