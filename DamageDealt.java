2f0ac6ed190b539e70e1b2411d6db97299427a22
Cook <s11098563@students.duvalschools.org>
2021-10-20 08:34:32 -0400

    [5B] v0.3
    Added main method statement.

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,7 +1,9 @@
-// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-20-2021, 8:28am, v0.1
+// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-20-2021, 8:32am, v0.3
 
 import java.util.Scanner;
 
 public class DamageDealt {
-    
+    public static void main(String[] args){
+
+    }
 }
