6e3af6d5507ecd073397fd8474e105222f869447
Cook <s11098563@students.duvalschools.org>
2021-10-20 08:57:53 -0400

    [5B] v0.5
    Getting user input for the weapon used.

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,4 +1,4 @@
-// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-20-2021, 8:35am, v0.4
+// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-20-2021, 8:57am, v0.5
 
 import java.util.Scanner;
 
@@ -6,7 +6,9 @@ public class DamageDealt {
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
 
-
+        // Weapon used
+        System.out.println("What weapon are you using?");
+        String weapon = myScanner.nextLine();
 
     }
 }
