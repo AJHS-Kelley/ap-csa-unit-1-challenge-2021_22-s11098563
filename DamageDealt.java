c5438dc7c0146be8349a3d4976c88092aa1cd906
Cook <s11098563@students.duvalschools.org>
2021-10-22 09:22:07 -0400

    [5B] v1.0
    Calculated and declared dps. Printed on screen dps during combat.

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,4 +1,4 @@
-// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-22-2021, 9:18am, v0.9
+// Connor Cook, AP CSA Unit 1 Challenge, Andrew Jackson High School, 10-22-2021, 9:21am, v1.0
 // Peer Debugging, Maxx Secrest
 import java.util.Scanner;
 
@@ -25,6 +25,10 @@ public class DamageDealt {
         // Total damage during combat
         Double totalDmg = numAttacks * damage;
         System.out.println("You did " + totalDmg + " damage to the " + monster + " with your " + weapon);
+
+        // Damage per second
+        Double dps = totalDmg / 4;
+        System.out.println("During combat you did " + dps + " damage per second.");
 
     }
 }
